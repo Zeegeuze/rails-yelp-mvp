66b8a228b743f2994c8265015a120fbea61f3cf4
Wouter Verhaeghe <wouter_verhaeghe@hotmail.com>
2018-10-31 12:08:39 +0100

    all green

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,46 @@
+class ReviewsController < ApplicationController
+  before_action :set_review, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @reviews = Review.all
+  end
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.Restaurant = Restaurant.find(params[:restaurant_id])
+    @review.save
+  end
+
+  def show
+
+  end
+
+  def destroy
+    @review.destroy
+    redirect_to review_path
+  end
+
+  def edit
+  end
+
+  def update
+    @review.update(review_params)
+    redirect_to review_path(@review)
+  end
+
+
+  private
+
+  def set_review
+    @review = review.find(params[:id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
+end
